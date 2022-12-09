bfabad4c48483de3f9b304f6c6c7b94288172f89
Pablo Vazquez <pvazquez@vandldigital.com>
2022-12-09 10:06:38 -0500

    feature: readme updates

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# MSC
+### MSC FULL-STACK APP
+
+
+##### Step - 1
+install latest MongoDB image using docker
+
+using command: 
+
+***docker run -d -p 27017:27017 --name test-mongo mongo:latest***
+
+##### Step - 2 
+cd into msc-api and run command:
+
+***npm run seed***
+
+this will seed the DB with contact data
+
+##### Step - 3 
+we need to create a text index so that our text search can work properly 
+in the terminal type mongo to open the mongo shell then run:
+
+***use contact***
+***db.contacts.createIndex({ "name": "text", "address": "text", "phoneNumber": "text", "email": "text" });***
+
+this will generate the index for our collection
+
+##### Step - 4
+run command to start the API
+
+npm run start
+
+##### Step - 5
+cd into mse-fe and run command
+
+***npm run dev***
+
+to run front-end app
--- msc-api/seeds/seed.contact.js
+++ msc-api/seeds/seed.contact.js
@@ -4,93 +4,93 @@ const connectToDB = require("../DB");
 
 const contacts = [
   {
-    name: "Jane Doe",
-    address: "1234 sw 123Str Miami FL, 33155",
-    phoneNumber: "305-345-6789",
-    email: "jane.doe1@gmail.com",
+    "name": "Jane Doe",
+    "address": "1234 sw 123Str Miami FL, 33155",
+    "phoneNumber": "305-345-6789",
+    "email": "jane.doe@gmail.com"
   },
   {
-    name: "Jane Doe",
-    address: "1234 sw 123Str Miami FL, 33155",
-    phoneNumber: "305-345-6789",
-    email: "jane.doe2@gmail.com",
+    "name": "jimmy jones",
+    "address": "1234 sw 123Str Miami FL, 33155",
+    "phoneNumber": "305-567-6734",
+    "email": "jimmy.jones2@gmail.com"
   },
   {
-    name: "Jane Doe",
-    address: "1234 sw 123Str Miami FL, 33155",
-    phoneNumber: "305-345-6789",
-    email: "jane.doe3@gmail.com",
+    "name": "james harding",
+    "address": "1234 sw 123Str Miami FL, 33155",
+    "phoneNumber": "305-567-1256",
+    "email": "james.harding@gmail.com"
   },
   {
-    name: "Jane Doe",
-    address: "1234 sw 123Str Miami FL, 33155",
-    phoneNumber: "305-345-6789",
-    email: "jane.doe4@gmail.com",
+    "name": "taylor swift",
+    "address": "1234 sw 123Str Miami FL, 33155",
+    "phoneNumber": "305-346-4865",
+    "email": "taytay4@gmail.com"
   },
   {
-    name: "Jane Doe",
-    address: "1234 sw 123Str Miami FL, 33155",
-    phoneNumber: "305-345-6789",
-    email: "jane.doe5@gmail.com",
+    "name": "luke skywalker",
+    "address": "1234 sw 123Str Miami FL, 33155",
+    "phoneNumber": "305-375-1136",
+    "email": "masterluke@gmail.com"
   },
   {
-    name: "Jane Doe",
-    address: "1234 sw 123Str Miami FL, 33155",
-    phoneNumber: "305-345-6789",
-    email: "jane.doe6@gmail.com",
+    "name": "darth vader",
+    "address": "1234 sw 123Str Miami FL, 33155",
+    "phoneNumber": "305-236-4976",
+    "email": "jane.doe6@gmail.com"
   },
   {
-    name: "Jane Doe",
-    address: "1234 sw 123Str Miami FL, 33155",
-    phoneNumber: "305-345-6789",
-    email: "jane.doe7@gmail.com",
+    "name": "john jones",
+    "address": "1234 sw 123Str Miami FL, 33155",
+    "phoneNumber": "305-785-6569",
+    "email": "jjones@gmail.com"
   },
   {
-    name: "Jane Doe",
-    email: "jane.doe8@gmail.com",
+    "name": "james bond",
+    "email": "bondjamesbond@gmail.com"
   },
   {
-    name: "Jane Doe",
-    address: "1234 sw 123Str Miami FL, 33155",
-    phoneNumber: "305-345-6789",
-    email: "jane.doe9@gmail.com",
+    "name": "mark anthony",
+    "address": "1234 sw 123Str Miami FL, 33155",
+    "phoneNumber": "305-687-4559",
+    "email": "mark@gmail.com"
   },
   {
-    name: "Jane Doe",
-    address: "1234 sw 123Str Miami FL, 33155",
-    phoneNumber: "305-345-6789",
-    email: "jane.doe10@gmail.com",
+    "name": "elvis presley",
+    "address": "1234 sw 123Str Miami FL, 33155",
+    "phoneNumber": "305-597-6569",
+    "email": "theking10@gmail.com"
   },
   {
-    name: "Jane Doe",
-    address: "1234 sw 123Str Miami FL, 33155",
-    phoneNumber: "305-345-6789",
-    email: "jane.doe11@gmail.com",
+    "name": "Anakin Skywalker",
+    "address": "1234 sw 123Str Miami FL, 33155",
+    "phoneNumber": "305-265-9789",
+    "email": "a.skywalker@gmail.com"
   },
   {
-    name: "Jane Doe",
-    address: "1234 sw 123Str Miami FL, 33155",
-    phoneNumber: "305-345-6789",
-    email: "jane.doe12@gmail.com",
+    "name": "steve jobs",
+    "address": "1234 sw 123Str Miami FL, 33155",
+    "phoneNumber": "305-2345-6789",
+    "email": "steve.jobs12@gmail.com"
   },
   {
-    name: "Jane Doe",
-    address: "1234 sw 123Str Miami FL, 33155",
-    phoneNumber: "305-345-6789",
-    email: "jane.doe13@gmail.com",
+    "name": "steven jones",
+    "address": "1234 sw 123Str Miami FL, 33155",
+    "phoneNumber": "305-455-6789",
+    "email": "sjones@gmail.com"
   },
   {
-    name: "Jane Doe",
-    address: "1234 sw 123Str Miami FL, 33155",
-    phoneNumber: "305-345-6789",
-    email: "jane.doe14@gmail.com",
+    "name": "lauren davis",
+    "address": "1234 sw 123Str Miami FL, 33155",
+    "phoneNumber": "305-565-8899",
+    "email": "lauren.davis4@gmail.com"
   },
   {
-    name: "Jane Doe",
-    address: "1234 sw 123Str Miami FL, 33155",
-    phoneNumber: "305-345-6789",
-    email: "jane.doe15@gmail.com",
-  },
+    "name": "Leonardo DaVinci",
+    "address": "1234 sw 123Str Miami FL, 33155",
+    "phoneNumber": "305-215-6777",
+    "email": "davinci@gmail.com"
+  }
 ];
 
 async function seedContacts() {
